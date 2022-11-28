c8ce865bde260d305bf18195a223a072229c826a
Gabeinstein <gsoch@hotmail.com>
2022-11-27 21:47:01 -0500

    Update Estructuras.py

--- Estructuras.py
+++ Estructuras.py
@@ -1,2 +1,18 @@
-#Proyecto Estructuras de Datos
-#
+#Visualizacion calendario
+
+import numpy as np
+import calendar
+
+class Node():
+    def __init__(self, node_act):
+        self.actividad = node_act
+c = calendar.TextCalendar(calendar.SUNDAY)
+strn = c.formatmonth(2022,1)
+
+
+calendario = np.zeros((5,7))
+
+for i in range(30):
+    calendario.itemset(i,i+1)
+
+print(calendario)
