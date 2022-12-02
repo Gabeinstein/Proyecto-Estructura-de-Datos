db5181e2ca1b1e924966d0023d85ab8153cfd40e
Nicolas <Nicolas>
2022-12-02 12:54:51 -0500

    Feature: class activity

--- actividades.py
+++ actividades.py
@@ -1,1 +1,14 @@
-######
+class Activity:
+    def __init__(self, name, date, priority):
+        self.name = name,
+        self.date = date,
+        self.priority = priority
+    
+    def set_date(self, date):
+        self.date = date
+
+    def set_priority(self, priority):
+        self.priority = priority
+
+    def set_name(self, name):
+        self.name = name
